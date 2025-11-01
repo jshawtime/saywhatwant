959bbb3dd6418d0747907fa045bb82742122c399
pbosh <pbosh@users.noreply.github.com>
2025-11-01 04:37:28 -0700

    Add bookmark reminder to empty state

--- components/MessageList/EmptyState.tsx
+++ components/MessageList/EmptyState.tsx
@@ -32,7 +32,8 @@ export const EmptyState: React.FC<EmptyStateProps> = ({
           <>
             <div style={{ fontSize: '28px', marginBottom: '20px' }}>
               SAY WHAT YOU WANT...
-              <br />
+            </div>
+            <div style={{ fontSize: '20px', marginBottom: '20px' }}>
               IMPORTANT!! bookmark this page to save your conversations
             </div>
             <div style={{ fontSize: '11px', opacity: 0.5, marginTop: '10px' }}>
